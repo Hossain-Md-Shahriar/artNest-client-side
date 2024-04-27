842219efd8fb9bcefd00972e5a7fa8ea91117aa9
Hossain-Md-Shahriar <hasibshahriar1@gmail.com>
2024-04-27 18:46:24 +0600

    private route added

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({children}) => {
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  }
+
+  if(user) {
+    return children;
+  }
+
+  return <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
