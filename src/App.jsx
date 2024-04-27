da28f7f1ed81eac3c2f3e29f148103c7a9ae8c03
Hossain-Md-Shahriar <hasibshahriar1@gmail.com>
2024-04-27 10:59:27 +0600

    setup complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h3 className='text-2xl font-bold text-purple-700 btn'>Hello world</h3>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h3 className="text-2xl">This is Home</h3>
+    </div>
+  )
+}
+
+export default Home
