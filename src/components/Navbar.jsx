79888d66d5879fd5915f21996f5ac80f52728c26
Hossain-Md-Shahriar <hasibshahriar1@gmail.com>
2024-04-27 14:41:57 +0600

    logout functionality and toggle between login and logout added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { IoMoon } from "react-icons/io5";
 import { IoSunny } from "react-icons/io5";
 import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
 
 const Navbar = () => {
+  const { user, logOut } = useContext(AuthContext);
   const [dark, setDark] = useState(false);
+
+  const handleLogOut = () => {
+    logOut()
+      .then(() => console.log("user logged out"))
+      .catch((error) => console.error(error));
+  };
 
   const darkModeHandler = () => {
     setDark(!dark);
@@ -17,7 +25,11 @@ const Navbar = () => {
         <NavLink
           to="/"
           className={({ isActive }) =>
-            `hover:underline underline-offset-2 ${isActive ? "font-semibold text-blue-500" : "font-normal dark:text-white"}`
+            `hover:underline underline-offset-2 ${
+              isActive
+                ? "font-semibold text-blue-500"
+                : "font-normal dark:text-white"
+            }`
           }
         >
           Home
@@ -27,7 +39,11 @@ const Navbar = () => {
         <NavLink
           to="/allItems"
           className={({ isActive }) =>
-            `hover:underline underline-offset-2 ${isActive ? "font-semibold text-blue-500" : "font-normal dark:text-white"}`
+            `hover:underline underline-offset-2 ${
+              isActive
+                ? "font-semibold text-blue-500"
+                : "font-normal dark:text-white"
+            }`
           }
         >
           All Items
@@ -63,14 +79,53 @@ const Navbar = () => {
             {navlinks}
           </ul>
         </div>
-        <Link to="/" className="text-xl font-bold dark:text-white">daisyUI</Link>
+        <Link to="/" className="text-xl font-bold dark:text-white">
+          daisyUI
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="flex gap-4">{navlinks}</ul>
       </div>
       <div className="navbar-end space-x-2">
-        <NavLink className={({isActive}) => `btn ${isActive ? "bg-blue-400" : "bg-white"}`} to="/login">Login</NavLink>
-        <NavLink className={({isActive}) => `btn ${isActive ? "bg-blue-400" : "bg-white"}`} to="/register">Register</NavLink>
+        {user ? (
+          <div className="flex gap-3 items-center">
+            <div className="tooltip tooltip-bottom" data-tip={user.displayName}>
+              <div className="size-12 rounded-full overflow-hidden border border-[#989898]">
+                <img
+                  className="size-full object-cover object-center"
+                  src={user.photoURL}
+                  alt=""
+                />
+              </div>
+            </div>
+            <button
+              onClick={handleLogOut}
+              className="btn bg-blue-400"
+            >
+              Sign out
+            </button>
+          </div>
+        ) : (
+          <>
+            <NavLink
+              className={({ isActive }) =>
+                `btn ${isActive ? "bg-blue-400" : "bg-white"}`
+              }
+              to="/login"
+            >
+              Login
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                `btn ${isActive ? "bg-blue-400" : "bg-white"}`
+              }
+              to="/register"
+            >
+              Register
+            </NavLink>
+          </>
+        )}
+
         <button onClick={darkModeHandler} className="text-2xl dark:text-white">
           {dark ? <IoSunny /> : <IoMoon />}
         </button>
